f4cf1b3f5cdbbf3c7a2e84260205e3d151aa66df
guilherm3souza <guilherm3sou@gmail.com>
2021-08-30 22:32:48 -0200

    agora vai

---
+++ Jovemprogramador.py
@@ -0,0 +1,12 @@
+class Class_Estados:
+    uf = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+    estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+    escolhe_estados = input("Escolha um estado")
+
+    def __init__(self, uf, estados):
+        self.__uf = uf
+        self.__estados = estados
+
+    if escolhe_estados == "AC":
+        print("ACRE")
+
